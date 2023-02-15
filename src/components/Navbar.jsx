0c91ab6c9a3c73173b0311e6af6c093e2578659f
Vitor Butkus <vitorbutkus@gmail.com>
2023-02-15 15:15:58 -0300

    add references page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,142 +27,145 @@ export default function Navbar() {
   };
 
   return (
-    <div
-      className={
-        shadow
-          ? 'fixed w-full h-20 shadow-lg z-[100] ease-in duration-300 '
-          : 'fixed w-full h-20 z-[100] ease-in duration-200 '
-      }
-    >
-      <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
-        <Link href='/#home' scroll={false}>
-          {/* <Image
+    <>
+      <div className='fixed z-[99] shadow-md bg-slate-600 w-full h-[95px]'></div>
+      <div
+        className={
+          shadow
+            ? 'fixed w-full h-16 shadow-lg z-[100] ease-in duration-300 bg-slate-500'
+            : 'fixed w-full h-16 shadow-md z-[100] ease-in duration-200 bg-slate-500'
+        }
+      >
+        <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16 max-w-[1240px] mx-auto'>
+          <Link href='/#home' scroll={false}>
+            {/* <Image
             src='/../public/assets/img/logo.png'
             alt='logo'
             width={150}
             height={50}
           /> */}
-          {/* <div className='h-1 w-[200px] bg-slate-800'></div> */}
-          <div className='px-3 text-lg'>Cabanagem 200</div>
-        </Link>
-        <div>
-          <ul className='hidden md:flex'>
-            <Link href='/#home' scroll={false}>
-              <li className='ml-10 text-sm uppercase hover:border-b text-[#999999]'>
-                {t('navbar.home')}
-              </li>
-            </Link>
-            <Link href='/#about' scroll={false}>
-              <li className='ml-10 text-sm uppercase hover:border-b text-[#999999]'>
-                {t('navbar.references')}
-              </li>
-            </Link>
-            <Link href='/#skills' scroll={false}>
-              <li className='ml-10 text-sm uppercase hover:border-b text-[#999999]'>
-                {t('navbar.glossary')}
-              </li>
-            </Link>
-            <Link href='/#projects' scroll={false}>
-              <li className='ml-10 text-sm uppercase hover:border-b text-[#999999]'>
-                {t('navbar.press')}
-              </li>
-            </Link>
-
-            <Link href='/#contact' scroll={false}>
-              <li className='ml-10 text-sm uppercase hover:border-b text-[#999999]'>
-                {t('navbar.about')}
-              </li>
-            </Link>
-          </ul>
-          <div onClick={handleNav} className='md:hidden text-[#999999]'>
-            <AiOutlineMenu size={25} />
-          </div>
-        </div>
-      </div>
-
-      <div
-        className={
-          nav ? 'fixed md:hidden left-0 top-0 w-full h-full bg-black/70' : ''
-        }
-      >
-        <div
-          className={
-            nav
-              ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'
-              : 'fixed left-[-100%] p-10 ease-in duration-500'
-          }
-        >
+            {/* <div className='h-1 w-[200px] bg-slate-800'></div> */}
+            <div className='px-1 text-lg'>Cabanagem 200</div>
+          </Link>
           <div>
-            <div className='flex w-full items-center justify-between'>
-              <Link href='/#home' scroll={false}>
-                <Image
-                  src='/../public/assets/logo.png'
-                  width={60}
-                  height={35}
-                  alt='logo'
-                />
-              </Link>
-              <div
-                onClick={handleNav}
-                className='rounded-full shadow-md p-3 cursor-pointer'
-              >
-                <AiOutlineClose />
-              </div>
-            </div>
-            <div className='border-b border-gray-300 my-4'>
-              <p className='w-[85%] md:w-[90%] py-4'>cabanagem200</p>
-            </div>
-          </div>
-          <div className='py-4 flex flex-col'>
-            <ul className='uppercase'>
-              <Link href='/#home' scroll={false}>
-                <li onClick={() => setNav(false)} className='py-4 text-sm'>
+            <ul className='hidden md:flex'>
+              <Link href='/' scroll={false}>
+                <li className='ml-8 text-sm uppercase hover:border-b text-[#999999]'>
                   {t('navbar.home')}
                 </li>
               </Link>
-              <Link href='/#about' scroll={false}>
-                <li onClick={() => setNav(false)} className='py-4 text-sm'>
+              <Link href='/references' scroll={false}>
+                <li className='ml-8 text-sm uppercase hover:border-b text-[#999999]'>
                   {t('navbar.references')}
                 </li>
               </Link>
               <Link href='/#skills' scroll={false}>
-                <li onClick={() => setNav(false)} className='py-4 text-sm'>
+                <li className='ml-8 text-sm uppercase hover:border-b text-[#999999]'>
                   {t('navbar.glossary')}
                 </li>
               </Link>
               <Link href='/#projects' scroll={false}>
-                <li onClick={() => setNav(false)} className='py-4 text-sm'>
+                <li className='ml-8 text-sm uppercase hover:border-b text-[#999999]'>
                   {t('navbar.press')}
                 </li>
               </Link>
+
               <Link href='/#contact' scroll={false}>
-                <li onClick={() => setNav(false)} className='py-4 text-sm'>
+                <li className='ml-8 text-sm uppercase hover:border-b text-[#999999]'>
                   {t('navbar.about')}
                 </li>
               </Link>
             </ul>
-            <div className='pt-40'>
-              <p className='uppercase tracking-widest text-[#5651e5]'>
-                Let's connect
-              </p>
-              <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
-                <div className='rounded-full shadow-lg p-3 cursor-pointer hover:scale-105 ease-in duration-500'>
-                  <FaLinkedinIn />
+            <div onClick={handleNav} className='md:hidden text-[#999999]'>
+              <AiOutlineMenu size={25} />
+            </div>
+          </div>
+        </div>
+
+        <div
+          className={
+            nav ? 'fixed md:hidden left-0 top-0 w-full h-full bg-black/70' : ''
+          }
+        >
+          <div
+            className={
+              nav
+                ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'
+                : 'fixed left-[-100%] p-10 ease-in duration-500'
+            }
+          >
+            <div>
+              <div className='flex w-full items-center justify-between'>
+                <Link href='/#home' scroll={false}>
+                  <Image
+                    src='/../public/assets/logo.png'
+                    width={60}
+                    height={35}
+                    alt='logo'
+                  />
+                </Link>
+                <div
+                  onClick={handleNav}
+                  className='rounded-full shadow-md p-3 cursor-pointer'
+                >
+                  <AiOutlineClose />
                 </div>
-                <div className='rounded-full shadow-lg p-3 cursor-pointer hover:scale-105 ease-in duration-500'>
-                  <FaGithub />
-                </div>
-                <div className='rounded-full shadow-lg p-3 cursor-pointer hover:scale-105 ease-in duration-500'>
-                  <AiOutlineMail />
-                </div>
-                <div className='rounded-full shadow-lg p-3 cursor-pointer hover:scale-105 ease-in duration-500'>
-                  <BsFillPersonLinesFill />
+              </div>
+              <div className='border-b border-gray-300 my-4'>
+                <p className='w-[85%] md:w-[90%] py-4'>cabanagem200</p>
+              </div>
+            </div>
+            <div className='py-4 flex flex-col'>
+              <ul className='uppercase'>
+                <Link href='/#home' scroll={false}>
+                  <li onClick={() => setNav(false)} className='py-4 text-sm'>
+                    {t('navbar.home')}
+                  </li>
+                </Link>
+                <Link href='/#about' scroll={false}>
+                  <li onClick={() => setNav(false)} className='py-4 text-sm'>
+                    {t('navbar.references')}
+                  </li>
+                </Link>
+                <Link href='/#skills' scroll={false}>
+                  <li onClick={() => setNav(false)} className='py-4 text-sm'>
+                    {t('navbar.glossary')}
+                  </li>
+                </Link>
+                <Link href='/#projects' scroll={false}>
+                  <li onClick={() => setNav(false)} className='py-4 text-sm'>
+                    {t('navbar.press')}
+                  </li>
+                </Link>
+                <Link href='/#contact' scroll={false}>
+                  <li onClick={() => setNav(false)} className='py-4 text-sm'>
+                    {t('navbar.about')}
+                  </li>
+                </Link>
+              </ul>
+              <div className='pt-40'>
+                <p className='uppercase tracking-widest text-[#5651e5]'>
+                  Let's connect
+                </p>
+                <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
+                  <div className='rounded-full shadow-lg p-3 cursor-pointer hover:scale-105 ease-in duration-500'>
+                    <FaLinkedinIn />
+                  </div>
+                  <div className='rounded-full shadow-lg p-3 cursor-pointer hover:scale-105 ease-in duration-500'>
+                    <FaGithub />
+                  </div>
+                  <div className='rounded-full shadow-lg p-3 cursor-pointer hover:scale-105 ease-in duration-500'>
+                    <AiOutlineMail />
+                  </div>
+                  <div className='rounded-full shadow-lg p-3 cursor-pointer hover:scale-105 ease-in duration-500'>
+                    <BsFillPersonLinesFill />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
---
+++ src/pages/references.jsx
@@ -0,0 +1,19 @@
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
+export default function References() {
+  return (
+    <div className='w-full pt-[120px]'>
+      <div className='max-w-[1240px] mx-auto'>
+        <h2 className=''>References</h2>
+      </div>
+    </div>
+  );
+}
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common'])),
+    },
+  };
+}
