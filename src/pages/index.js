201a52a2e577e56efc61da65d328c83f4e073ede
Vitor Butkus <vitorbutkus@gmail.com>
2023-03-31 15:40:15 -0300

    add head with og

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,48 @@ export default function Home() {
     <>
       <Head>
         <title>cabanagem200</title>
-        <meta name='description' content='Pará, Brasil, 1835' />
+        <meta
+          name='description'
+          content='Cabanagem 200 é um site que mostra de forma permanente arquivos do conjunto de trabalhos Nheenga Cabana, apresentando com destaque a edição extraordinária do Jornal Pessoal de Lúcio Flávio Pinto.'
+        />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
+
+        <meta charset='utf-8' />
+        <meta name='viewport' content='width=device-width' />
+
+        <meta
+          name='author'
+          content='Giseli Vasconcelos / Pedro Victor Brandão'
+        />
+        <meta
+          name='description'
+          content='Cabanagem 200 é um site que mostra de forma permanente arquivos do conjunto de trabalhos Nheenga Cabana, apresentando com destaque a edição extraordinária do Jornal Pessoal de Lúcio Flávio Pinto'
+        />
+        <meta
+          name='keywords'
+          content='giselivasconcelos, pedrovictorbrandão, cabanagem'
+        />
+
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:site' content='@pierresacoman' />
+        <meta name='twitter:title' content='Cabanagem 200' />
+        <meta
+          name='twitter:description'
+          content='Cabanagem 200 é um site que mostra de forma permanente arquivos do conjunto de trabalhos Nheenga Cabana, apresentando com destaque a edição extraordinária do Jornal Pessoal de Lúcio Flávio Pinto.'
+        />
+        <meta name='twitter:image' content='./img/ico-provisorio.jpeg' />
+
+        <meta property='og:title' content='Cabanagem 200' />
+        <meta
+          property='og:description'
+          content='Cabanagem 200 é um site que mostra de forma permanente arquivos do conjunto de trabalhos Nheenga Cabana, apresentando com destaque a edição extraordinária do Jornal Pessoal de Lúcio Flávio Pinto.'
+        />
+        <meta property='og:image' content='./img/ico-provisorio.jpeg' />
+        <meta property='og:image:width' content='1200' />
+        <meta property='og:image:height' content='630' />
+        <meta property='og:type' content='article' />
+        <meta property='og:locale' content='en_US' />
       </Head>
     </>
   );
