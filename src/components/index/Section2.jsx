2740e1064ddb7d88afaced1a81349a16e1f7c118
Vitor Butkus <vitorbutkus@gmail.com>
2023-03-31 19:12:54 -0300

    add rel="noreferrer" to link

--- src/components/index/Section2.jsx
+++ src/components/index/Section2.jsx
@@ -6,9 +6,9 @@ export default function Section2() {
           <span className='tracking-wider font-stanleybold text-[15px]'>
             cabanagem200
           </span>{' '}
-          apresenta <span className='italic'>Nheenga Cabana</span>, um conjunto
-          de trabalhos de Giseli Vasconcelos e Pedro Victor Brandão que trata da
-          Revolução da Cabanagem ocorrida no período regencial, quando
+          apresenta <span className='font-bold'>Nheenga Cabana</span>, um
+          conjunto de trabalhos de Giseli Vasconcelos e Pedro Victor Brandão que
+          trata da Revolução da Cabanagem ocorrida no período regencial, quando
           insurgências eclodiram no Brasil durante o século XIX. O projeto se
           deu a partir da leitura dos escritos e arquivos sobre a Cabanagem do
           jornalista e sociólogo paraense Lúcio Flávio Pinto, postados nos
@@ -17,18 +17,19 @@ export default function Section2() {
             className='text-slate-700 tracking-tight font-stanley'
             href='https://cabanagem180.wordpress.com'
             target='_blank'
+            rel='noreferrer'
           >
             https://cabanagem180.wordpress.com
           </a>
           .
         </p>{' '}
         <p>
-          Ao longo do processo, editamos uma edição extraordinária do Jornal
-          Pessoal de Lúcio sobre esse período e co-criamos imagens com versões
-          iniciais de tecnologias recentes de aprendizagem de máquina
-          (MidJourney, DALL-E 2, Stable Difusion) para criar uma iconografia
-          contemporânea sobre uma revolução que colocou, pela primeira vez, o
-          povo no poder.
+          Ao longo do processo, editamos uma edição extraordinária do{' '}
+          <span className='font-bold'>Jornal Pessoal</span> de Lúcio sobre esse
+          período e co-criamos imagens com versões iniciais de tecnologias
+          recentes de aprendizagem de máquina (MidJourney, DALL-E 2, Stable
+          Difusion) para criar uma iconografia contemporânea sobre uma revolução
+          que colocou, pela primeira vez, o povo no poder.
         </p>
         <p>
           Reunimos aqui os materiais apresentados na instalação com painéis
