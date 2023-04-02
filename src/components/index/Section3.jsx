107d36ed4dee9099a333444cc673ccf1538add4b
Vitor Butkus <vitorbutkus@gmail.com>
2023-04-02 14:58:11 -0300

    adjust stick title in Section3

--- src/components/index/Section3.jsx
+++ src/components/index/Section3.jsx
@@ -6,10 +6,10 @@ export default function Section3() {
   return (
     <div className='flex'>
       <div
-        className='w-20 h-[calc(100vh-3.5rem)] pt-6 flex items-center sticky top-14'
+        className='w-14 md:w-20 h-[calc(100vh-3.5rem)] pt-6 flex items-center sticky top-14'
         style={{ writingMode: 'vertical-rl', height: 'calc(100vh-20px)' }}
       >
-        <div className='max-h-[500px] text-sm'>
+        <div className='max-h-[350px] md:max-h-[500px] text-xs md:text-sm'>
           <span className='font-bold'>Nheenga Cabana</span>, 2022 Instalação com
           painéis expográficos, impressões em jato de tinta e tiragem offset de
           jornal
