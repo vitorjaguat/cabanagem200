1bf29d068cf762caed46db1ec6b46afb41596259
Vitor Butkus <vitorbutkus@gmail.com>
2023-04-03 17:22:59 -0300

    add responsivity to Section3, refactor Section3Item

--- src/components/index/Section3.jsx
+++ src/components/index/Section3.jsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import { useInView } from 'react-intersection-observer';
 import inst1Img from '../../../public/img/inst1.jpg';
 import inst2Img from '../../../public/img/inst2.jpg';
 import inst3Img from '../../../public/img/inst3.jpg';
@@ -7,9 +6,9 @@ import obr1Img from '../../../public/img/obr1.jpg';
 import obr2Img from '../../../public/img/obr2.jpg';
 import obr3Img from '../../../public/img/obr3.jpg';
 import { useEffect, useRef, useState } from 'react';
+import Section3Item from './Section3Item';
 
 export default function Section3() {
-  const { ref, inView } = useInView();
   const img1Ref = useRef();
   const [showTitle1, setShowTitle1] = useState(false);
 
@@ -17,27 +16,6 @@ export default function Section3() {
     x: 0,
     y: 0,
   });
-
-  //   useEffect(() => {
-  //     const mouseMove = (e) => {
-  //       //   console.log(e.target);
-  //       if (e.target !== img1Ref.current) return;
-
-  //       setMousePosition((currPosition) => {
-  //         return {
-  //           x: e.clientX,
-  //           y: e.clientY,
-  //         };
-  //       });
-  //       console.log(mousePosition);
-  //     };
-
-  //     window.addEventListener('mousemove', mouseMove);
-
-  //     return () => {
-  //       window.removeEventListener('mousemove', mouseMove);
-  //     };
-  //   }, []);
 
   const handleMouseEnter1 = (e) => {
     if (e.currentTarget !== img1Ref.current) return;
@@ -50,51 +28,146 @@ export default function Section3() {
 
   return (
     <div className='flex'>
+      {/* STICKY TITLE */}
       <div
-        className='w-14 md:w-20 h-[calc(100vh-3rem)] pt-6 flex items-center sticky top-14'
+        className='w-14 md:w-20 h-[calc(100vh-2.5rem)] md:h-[calc(100vh-3rem)] flex items-center sticky top-14 md:pt-4'
         style={{ writingMode: 'vertical-rl', height: 'calc(100vh-20px)' }}
       >
-        <div className='min-h-[350px] max-h-[350px] md:max-h-[500px] md:min-h-[500px] text-xs md:text-sm'>
+        <div className='min-h-[350px] max-h-[350px] md:max-h-[500px] md:min-h-[500px] text-[11px] md:text-sm px-3 md:px-4'>
           <span className='font-bold'>Nheenga Cabana</span>, 2022 Instalação com
           painéis expográficos, impressões em jato de tinta e tiragem offset de
           jornal
         </div>
       </div>
 
-      <div className='h-[3000px] w-full flex flex-col items-center pt-6'>
-        <div
-          className='flex gap-4 relative cursor-none'
-          ref={img1Ref}
-          onMouseMove={handleMouseEnter1}
-          onMouseLeave={() => setMousePosition({ x: 0, y: 0 })}
-        >
-          <div className=' cursor-none'>
-            <Image src={inst1Img} alt='7 de Janeiro de 1835' />
-          </div>
-
-          <div className='pr-4 cursor-none'>
-            <Image src={obr1Img} alt='7 de Janeiro de 1835' />
-          </div>
-
+      <div className='flex flex-col items-center'>
+        {/* IMAGE 1 */}
+        <Section3Item
+          obrImg={obr1Img}
+          instImg={inst1Img}
+          title='7 de Janeiro de 1835'
+          media='Impressão em jato de tinta sobre papel de algodão a partir de imagens geradas por aprendizagem de máquina'
+          measure='110x110cm'
+          date='2022'
+        />
+        <Section3Item
+          obrImg={obr2Img}
+          instImg={inst2Img}
+          title='256 cabanxs'
+          media='Impressão em jato de tinta sobre papel de algodão a partir de imagens geradas por aprendizagem de máquina'
+          measure='110x110cm'
+          date='2022'
+        />
+        <Section3Item
+          obrImg={obr3Img}
+          instImg={inst3Img}
+          title='Sonho Cabano'
+          media='Impressão em jato de tinta sobre papel de algodão a partir de imagens geradas por aprendizagem de máquina'
+          measure='110x110cm'
+          date='2022'
+        />
+        {/* <div className='w-full flex flex-col items-center'>
           <div
-            className='bg-[#00000090] py-2 pr-3 pl-4 max-w-[200px] text-slate-200 tracking-wide rounded-sm absolute cursor-none'
-            style={{
-              display: mousePosition.x !== 0 ? 'block' : 'none',
-              top: mousePosition.y,
-              left: mousePosition.x,
-            }}
+            className='flex flex-col md:flex-row gap-2 md:gap-4 relative cursor-none'
+            ref={img1Ref}
+            onMouseMove={handleMouseEnter1}
+            onMouseLeave={() => setMousePosition({ x: 0, y: 0 })}
           >
-            <p className='font-stanleybold text-[13px] leading-5'>
-              7 de Janeiro de 1835
-            </p>
-            <p className='leading-4 text-[13px]'>
-              Impressão em jato de tinta sobre papel de algodão a partir de
-              imagens geradas por aprendizagem de máquina
-            </p>
-            <p className='text-[13px] leading-4'>110x110cm</p>
-            <p className='text-[13px] leading-4'>2022</p>
+            <div className='cursor-none'>
+              <Image src={obr1Img} alt='7 de Janeiro de 1835' />
+            </div>
+            <div className='flex md:block pr-2 md:pr-4 cursor-none'>
+              <Image
+                src={inst1Img}
+                className='w-[200px] md:w-auto'
+                alt='7 de Janeiro de 1835'
+              />
+              <div className='md:hidden pl-2 flex flex-col justify-end'>
+                <p className='font-stanleybold text-[10px] leading-tight'>
+                  7 de Janeiro de 1835
+                </p>
+                <p className='leading-0 text-[9px] leading-tight'>
+                  Impressão em jato de tinta sobre papel de algodão a partir de
+                  imagens geradas por aprendizagem de máquina
+                </p>
+                <p className='text-[9px] leading-tight'>110x110cm</p>
+                <p className='text-[9px] leading-tight'>2022</p>
+              </div>
+            </div>
+
+            <div
+              className='bg-[#00000090] py-2 pr-3 pl-4 max-w-[200px] text-slate-200 tracking-wide rounded-sm absolute cursor-none'
+              style={{
+                display: mousePosition.x !== 0 ? 'block' : 'none',
+                top: mousePosition.y,
+                left: mousePosition.x,
+              }}
+            >
+              <p className='font-stanleybold text-[13px] leading-5'>
+                7 de Janeiro de 1835
+              </p>
+              <p className='leading-4 text-[13px]'>
+                Impressão em jato de tinta sobre papel de algodão a partir de
+                imagens geradas por aprendizagem de máquina
+              </p>
+              <p className='text-[13px] leading-4'>110x110cm</p>
+              <p className='text-[13px] leading-4'>2022</p>
+            </div>
           </div>
-        </div>
+          <div className='h-4 md:h-16 w-[80%] border-b-[1px] border-dotted border-black'></div>
+        </div> */}
+
+        {/* IMAGE 2 */}
+        {/* <div className='pt-4 md:pt-16 w-full flex flex-col items-center'>
+          <div
+            className='flex flex-col md:flex-row gap-2 md:gap-4 relative cursor-none'
+            ref={img1Ref}
+            onMouseMove={handleMouseEnter1}
+            onMouseLeave={() => setMousePosition({ x: 0, y: 0 })}
+          >
+            <div className='cursor-none'>
+              <Image src={obr1Img} alt='7 de Janeiro de 1835' />
+            </div>
+            <div className='flex md:block pr-2 md:pr-4 cursor-none'>
+              <Image
+                src={inst1Img}
+                className='w-[200px] md:w-auto'
+                alt='7 de Janeiro de 1835'
+              />
+              <div className='md:hidden pl-2 flex flex-col justify-end'>
+                <p className='font-stanleybold text-[10px] leading-tight'>
+                  7 de Janeiro de 1835
+                </p>
+                <p className='leading-0 text-[9px] leading-tight'>
+                  Impressão em jato de tinta sobre papel de algodão a partir de
+                  imagens geradas por aprendizagem de máquina
+                </p>
+                <p className='text-[9px] leading-tight'>110x110cm</p>
+                <p className='text-[9px] leading-tight'>2022</p>
+              </div>
+            </div>
+
+            <div
+              className='bg-[#00000090] py-2 pr-3 pl-4 max-w-[200px] text-slate-200 tracking-wide rounded-sm absolute cursor-none'
+              style={{
+                display: mousePosition.x !== 0 ? 'block' : 'none',
+                top: mousePosition.y,
+                left: mousePosition.x,
+              }}
+            >
+              <p className='font-stanleybold text-[13px] leading-5'>
+                7 de Janeiro de 1835
+              </p>
+              <p className='leading-4 text-[13px]'>
+                Impressão em jato de tinta sobre papel de algodão a partir de
+                imagens geradas por aprendizagem de máquina
+              </p>
+              <p className='text-[13px] leading-4'>110x110cm</p>
+              <p className='text-[13px] leading-4'>2022</p>
+            </div>
+          </div>
+          <div className='h-4 md:h-16 w-[80%] border-b-[1px] border-dotted border-black'></div>
+        </div> */}
       </div>
     </div>
   );
---
+++ src/components/index/Section3Item.jsx
@@ -0,0 +1,71 @@
+import Image from 'next/image';
+import { useEffect, useRef, useState } from 'react';
+
+export default function Section3Item({
+  obrImg,
+  instImg,
+  title,
+  media,
+  measure,
+  date,
+}) {
+  const img1Ref = useRef();
+  const [showTitle1, setShowTitle1] = useState(false);
+
+  const [mousePosition, setMousePosition] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  const handleMouseEnter1 = (e) => {
+    if (e.currentTarget !== img1Ref.current) return;
+    setMousePosition({
+      x: e.clientX - img1Ref.current.getBoundingClientRect().left,
+      y: e.clientY - img1Ref.current.getBoundingClientRect().top,
+    });
+  };
+  console.log(mousePosition);
+
+  return (
+    <div className='w-full flex flex-col items-center pb-16'>
+      <div
+        className='flex flex-col md:flex-row gap-2 md:gap-4 relative cursor-none'
+        ref={img1Ref}
+        onMouseMove={handleMouseEnter1}
+        onMouseLeave={() => setMousePosition({ x: 0, y: 0 })}
+      >
+        <div className='cursor-none pr-2 md:pr-0'>
+          <Image src={obrImg} alt='7 de Janeiro de 1835' />
+        </div>
+        <div className='flex md:block pr-2 md:pr-4 cursor-none'>
+          <Image src={instImg} className='w-[200px] md:w-auto' alt={title} />
+          {/* Ficha técnica MOBILE */}
+          <div className='md:hidden pl-2 flex flex-col justify-end'>
+            <p className='font-stanleybold text-[10px] leading-tight'>
+              {title}
+            </p>
+            <p className='leading-0 text-[9px] leading-tight'>{media}</p>
+            <p className='text-[9px] leading-tight'>{measure}</p>
+            <p className='text-[9px] leading-tight'>{date}</p>
+          </div>
+        </div>
+
+        {/* Ficha técnica DESKTOP */}
+        <div
+          className='bg-[#00000090] md:py-2 md:pr-3 pl-4 h-0 md:h-auto md:max-w-[200px] text-slate-200 tracking-wide rounded-sm absolute cursor-none overflow-hidden'
+          style={{
+            display: mousePosition.x !== 0 ? 'block' : 'none',
+            top: mousePosition.y,
+            left: mousePosition.x,
+          }}
+        >
+          <p className='font-stanleybold text-[13px] leading-5'>{title}</p>
+          <p className='leading-4 text-[13px]'>{media}</p>
+          <p className='text-[13px] leading-4'>{measure}</p>
+          <p className='text-[13px] leading-4'>{date}</p>
+        </div>
+      </div>
+      {/* <div className='h-4 md:h-16 w-[80%] border-b-[1px] border-dotted border-black'></div> */}
+    </div>
+  );
+}
