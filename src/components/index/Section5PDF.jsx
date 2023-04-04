2428106a25ed9aeb4c8e339e73e41bdfacad57a2
Vitor Butkus <vitorbutkus@gmail.com>
2023-04-04 18:33:48 -0300

    add pdf

--- src/components/index/Section5PDF.jsx
+++ src/components/index/Section5PDF.jsx
@@ -12,7 +12,7 @@ export default function Section5PDF() {
   }
 
   return (
-    <div className='flex mt-20'>
+    <div className='flex mt-[300px]'>
       {/* STICKY TITLE */}
       <div
         className='w-14 md:w-20 min-h-[350px] max-h-[350px] md:max-h-[500px] md:min-h-[500px] flex items-center sticky top-14 md:pt-4'
@@ -23,19 +23,43 @@ export default function Section5PDF() {
         </div>
       </div>
 
-      <div className='w-full'>
-        <div className='mx-auto max-w-[1000px] py-[300px]'>
+      <div className='w-full flex justify-center items-center'>
+        <div className='w-fit'>
           <Document
             file='/assets/jornal-pessoal-edext.pdf'
             onLoadSuccess={onDocumentLoadSuccess}
+            // className='mx-auto'
+            // style={{ display: 'flex', alignItems: 'center' }}
           >
-            <Page pageNumber={pageNumber} pageIndex={0} />
+            <Page
+              renderTextLayer={false}
+              pageNumber={pageNumber}
+              pageIndex={0}
+              // style={{ display: 'flex', alignItems: 'center' }}
+            />
           </Document>
           {/* <iframe
             src='/assets/jornal-pessoal-edext.pdf'
             // frameborder='0'
             className='w-[600px] h-[900px]'
           ></iframe> */}
+        </div>
+
+        <div className='h-full flex flex-col justify-center'>
+          <button
+            className=''
+            disabled={pageNumber <= 1}
+            onClick={() => setPageNumber((curr) => curr - 1)}
+          >
+            previous page
+          </button>
+          <button
+            className=''
+            disabled={pageNumber >= numPages}
+            onClick={() => setPageNumber((curr) => curr + 1)}
+          >
+            next page
+          </button>
         </div>
       </div>
     </div>
