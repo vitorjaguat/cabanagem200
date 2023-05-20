5b3479063b45053feb0acc949f38c4009dfa2615
Vitor Butkus <vitorbutkus@gmail.com>
2023-05-20 13:04:22 -0300

    update about.jsx text

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -14,31 +14,43 @@ export default function About() {
         <div className='mb-4'>
           <span className='font-stanleybold'>cabanagem200</span> é um site que
           mostra de forma permanente arquivos do conjunto de trabalhos{' '}
-          <span className='font-stanleybold'>Nheenga Cabana</span>, apresentando
-          com destaque a edição extraordinária do{' '}
-          <span className='font-stanleybold'>Jornal Pessoal</span> de Lúcio
-          Flávio Pinto, o editorial escrito por nós, a ficha técnica de todo o
-          projeto, materiais de imprensa, os arquivos de imagem co-criados com
-          inteligências artificiais exibidos no MAM-Rio e também uma seleção de
-          arquivos de imagens inéditos. O site exibe conteúdos de mídia
-          hospedados com Arweave de forma a preservar sua permanência.
+          <span className='font-stanleyitalic'>Nheenga Cabana</span>,
+          apresentando com destaque a edição extraordinária do{' '}
+          <span className='font-stanleyitalic'>Jornal Pessoal</span> de Lúcio
+          Flávio Pinto, a ficha técnica de todo o projeto, materiais de
+          imprensa, os arquivos de imagem co-criados com inteligências
+          artificiais exibidos no MAM-Rio e também uma seleção de arquivos de
+          imagens inéditos. O site exibe conteúdos de mídia hospedados com IPFS
+          de forma a preservar sua permanência.
         </div>
         <div className=''>
-          <span className='font-stanleybold'>Nheenga Cabana</span> é uma série
+          <span className='font-stanleyitalic'>Nheenga Cabana</span> é uma série
           de obras comissionadas pelo Museu de Arte Moderna do Rio de Janeiro
-          para a exposição{' '}
-          <span className='font-stanleybold'>
+          para a exposição coletiva{' '}
+          <a
+            className='text-slate-700 tracking-tight font-stanleybold hover:underline'
+            href='https://mam.rio/programacao/atos-de-revolta/'
+            target='_blank'
+            rel='noreferrer'
+          >
             Atos de revolta: outros imaginários sobre independência
-          </span>
+          </a>
           , uma curadoria de Beatriz Lemos, Keyna Eleison, Pablo Lafuente e
-          Thiago de Paula Souza, realizada entre setembro de 2022 a maio de
-          2023.
+          Thiago de Paula Souza, realizada entre setembro de 2022 e maio de
+          2023. Também participaram da coletiva com obras comissionadas Arissana
+          Pataxó, Ana Lira, Elian Almeida, Gê Viana, Gustavo Caboco Wapichana
+          com Roseane Cadete Wapichana, Marcela Cantuária e Tiago Sant’Ana;
+          junto a obras recentes de Arjan Martins, Glicéria Tupinambá, Paulo
+          Nazareth e Thiago Martins de Melo e Luana Vitra - ao lado de uma
+          seleção de objetos dos séculos 18 e 19, vindos dos acervos do Museu da
+          Inconfidência e do Museu Histórico Nacional; e oito pinturas do
+          artista gaúcho Glauco Rodrigues, pertencentes ao acervo do MAM-Rio.
         </div>
         <div className='mt-20 mb-8 text-center'>
           <div className='font-stanleybold'>cabanagem200.xyz</div>
           <div className=''>Desenvolvimento web: Vitor Butkus</div>
         </div>
-        <div className='text-center'>
+        <div className='text-center mb-8'>
           <div className='font-stanleybold'>
             Jornal Pessoal Edição Extraordinária
           </div>
@@ -50,6 +62,68 @@ export default function About() {
           <div className=''>Design e Diagramação: Tatiana Podlubny</div>
           <div className=''>Impressão: Gráfica Rotativa</div>
           <div className=''>Tiragem: 5.000 cópias</div>
+        </div>
+        <div className='text-center'>
+          <div className='font-stanleybold'>
+            Ferramentas de criação de imagem utilizadas
+          </div>
+          <div className=''>
+            DALL-E 2 (acessível em{' '}
+            <a
+              className='text-slate-700 tracking-tight font-stanleybold hover:underline'
+              href='https://labs.openai.com/'
+              target='_blank'
+              rel='noreferrer'
+            >
+              https://labs.openai.com/
+            </a>
+            )
+          </div>
+          <div className=''>
+            MidJourney v2 e v3 (atualmente v5, acessível em{' '}
+            <a
+              className='text-slate-700 tracking-tight font-stanleybold hover:underline'
+              href='https://www.midjourney.com/'
+              target='_blank'
+              rel='noreferrer'
+            >
+              https://www.midjourney.com/
+            </a>
+            )
+          </div>
+          <div className=''>
+            Stable Diffusion v1.4 (Modelo local acessível em{' '}
+            <a
+              className='text-slate-700 tracking-tight font-stanleybold hover:underline'
+              href='https://github.com/CompVis/stable-diffusion'
+              target='_blank'
+              rel='noreferrer'
+            >
+              https://github.com/CompVis/stable-diffusion
+            </a>
+            ; atualmente v2.1, acessível em{' '}
+            <a
+              className='text-slate-700 tracking-tight font-stanleybold hover:underline'
+              href='https://github.com/Stability-AI/StableStudio/'
+              target='_blank'
+              rel='noreferrer'
+            >
+              https://github.com/Stability-AI/StableStudio/
+            </a>
+            )
+          </div>
+          <div className=''>
+            Waifu2x v3.92.01 (atualmente v3.101.01, acessível em{' '}
+            <a
+              className='text-slate-700 tracking-tight font-stanleybold hover:underline'
+              href='https://github.com/AaronFeng753/Waifu2x-Extension-GUI/'
+              target='_blank'
+              rel='noreferrer'
+            >
+              https://github.com/AaronFeng753/Waifu2x-Extension-GUI/
+            </a>
+            ){' '}
+          </div>
         </div>
       </div>
     </div>
