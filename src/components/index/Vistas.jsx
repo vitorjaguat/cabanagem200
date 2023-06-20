835a4555c0c8d149bec8f4f973a742ec9c6c6565
Vitor Butkus <vitorbutkus@gmail.com>
2023-06-20 14:04:37 -0300

    adjust gap in Vistas

--- src/components/index/Vistas.jsx
+++ src/components/index/Vistas.jsx
@@ -68,7 +68,7 @@ export default function Vistas() {
           </Link>
           .
         </div>
-        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 md:gap-4'>
           {imgData.map((img, i) => (
             <div className='' key={i}>
               <Image
